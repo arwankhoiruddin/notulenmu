b83214ab2c0bdc0d75e70634b105a2d7df3aaefe
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2024-04-03 15:11:16 +0700

    Update about NotulenMu

--- submenu/about_notulen.php
+++ submenu/about_notulen.php
@@ -1,4 +1,6 @@
 <?php
 function notulenmu_page(){
-    echo "About";
+    echo "<h1>Tentang NotulenMu</h1>";
+    echo "<p>NotulenMu adalah plugin yang digunakan untuk mencatat notulen rapat di wilayah, daerah, cabang dan ranting</p>";
+    echo "<p>Plugin ini dikembangkan oleh <a href='https://mandatech.co.id'>Arwan Ahmad Khoiruddin</a></p>";
 }
