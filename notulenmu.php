f4e10769e7c4b55d0d322313748b7252456e99bd
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2025-07-11 22:06:05 +0700

    Hilangkan menu tambah notulen

--- notulenmu.php
+++ notulenmu.php
@@ -53,8 +53,8 @@ function notulenmu_menu()
         add_submenu_page('notulenmu', 'Setting Notulen', 'Setting Notulen', 'read', 'notulenmu-settings', 'notulenmu_settings_page');
         add_submenu_page('__hidden', 'Data Pengurus', 'Tambah Pengurus', 'read', 'pengurus-add', 'pengurus_add_page');
         add_submenu_page('notulenmu', 'Pengurus', 'Pengurus', 'read', 'pengurus-list', 'pengurus_list_page');
-        add_submenu_page('notulenmu', 'Tambah Notulen', 'Tambah Notulen', 'read', 'notulenmu-add', 'notulenmu_add_page');
-        add_submenu_page('notulenmu', 'List Notulen', 'Daftar Notulen', 'read', 'notulenmu-list', 'notulenmu_list_page');
+        add_submenu_page('__hidden', 'Tambah Notulen', 'Tambah Notulen', 'read', 'notulenmu-add', 'notulenmu_add_page');
+        add_submenu_page('notulenmu', 'Notulen', 'Notulen', 'read', 'notulenmu-list', 'notulenmu_list_page');
         add_submenu_page('notulenmu', 'Tambah Kegiatan', 'Tambah Kegiatan', 'read', 'kegiatanmu-add', 'tambah_kegiatan_page');
         add_submenu_page('notulenmu', 'List Kegiatan', 'Daftar Kegiatan', 'read', 'kegiatanmu-list', 'kegiatanmu_list_page');
         add_submenu_page('notulenmu', 'View Notulen', '', 'read', 'notulenmu-view', 'notulenmu_view_page');
